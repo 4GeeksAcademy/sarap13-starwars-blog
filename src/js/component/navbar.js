9616cd3e165da6a674be4c5a423c7381d8b02e2b
Sara Parra <131387591+sarap13@users.noreply.github.com>
2024-01-11 21:26:36 +0000

    WIP navbar fixed and mb done

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -12,24 +12,27 @@ export const Navbar = () => {
 	const { store, actions } = useContext(Context)
 
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<img className="card-img-top w-25 mx-5" src="https://visualpharm.com/assets/195/Star%20Wars-595b40b85ba036ed117de471.svg" />
-			</Link>
-			<div className="">
-				<Link to="/demo">
-					<div className="btn-group" role="group">
-						<button id="btnGroupDrop1" type="button" className="btn btn-primary dropdown-toggle mx-3" data-bs-toggle="dropdown" aria-expanded="false">
-							Favoritos
-						</button>
-						<ul className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-							{store.favorites.map((item, index) => (
-								<li key={index} ><a className="dropdown-item" href="#">{item}</a></li>
-							))}
-						</ul>
-					</div>
+		<div className="container-fluid p-0">
+			<nav className="navbar navbar-light bg-light mb-3">
+				<Link to="/">
+					<img className="card-img-top w-25 mx-5" src="https://visualpharm.com/assets/195/Star%20Wars-595b40b85ba036ed117de471.svg" />
 				</Link>
-			</div>
-		</nav >
+				<div className="">
+					<Link to="/demo">
+						<div className="btn-group" role="group">
+							<button id="btnGroupDrop1" type="button" className="btn btn-primary dropdown-toggle mx-3" data-bs-toggle="dropdown" aria-expanded="false">
+								Favoritos
+							</button>
+							<ul className="dropdown-menu" aria-labelledby="btnGroupDrop1">
+								{store.favorites.map((item, index) => (
+									<li key={index} ><a className="dropdown-item" href="#">{item}</a></li>
+								))}
+							</ul>
+						</div>
+					</Link>
+				</div>
+
+			</nav >
+		</div >
 	);
 };
